9f761612a25ef6c3188de5b7997b9d9bf599b885
sjj0624 <sjj0624@ATL42SJJ080921.local>
2018-09-26 16:06:41 -0400

    Version 2: Lacking writeability to BQ

--- src/main/java/dao/RFIDEvent.java
+++ src/main/java/dao/RFIDEvent.java
@@ -11,34 +11,41 @@ public class RFIDEvent {
     private String tagId;
     private String receiverId;
     private String storeNumber;
-    private String eventTime;
-    private String signal;
-
-
+    private DateTime eventTime;
+    private Integer signal;
     private String location;
     private Boolean exitReader;
     private String upc;
     private String productName;
     private Double currRetailAmt;
     private Boolean asciiTag;
-
     private int checkedCounter;
     private Boolean matched;
 
 
-    public RFIDEvent(String tagId, String receiverId, String storeNumber, String eventTime, String signal) {
+    public RFIDEvent(String tagId, String receiverId, String storeNumber, DateTime eventTime, Integer signal, String location,
+                     Boolean exitReader, String upc, String productName, Double currRetailAmt, Boolean asciiTag, int checkedCounter, Boolean matched) {
         this.tagId = tagId;
         this.receiverId = receiverId;
         this.storeNumber = storeNumber;
         this.eventTime = eventTime;
         this.signal = signal;
+        this.location = location;
+        this.exitReader = exitReader;
+        this.upc = upc;
+        this.productName = productName;
+        this.currRetailAmt = currRetailAmt;
+        this.asciiTag = asciiTag;
+        this.checkedCounter = checkedCounter;
+        this.matched = matched;
+
     }
 
-    public String getSignal() {
+    public Integer getSignal() {
         return signal;
     }
 
-    public void setSignal(String signal) {
+    public void setSignal(Integer signal) {
         this.signal = signal;
     }
 
@@ -50,11 +57,11 @@ public class RFIDEvent {
         this.tagId = tagId;
     }
 
-    public String getEventTime() {
+    public DateTime getEventTime() {
         return eventTime;
     }
 
-    public void setEventTime(String eventTime) {
+    public void setEventTime(DateTime eventTime) {
         this.eventTime = eventTime;
     }
 
@@ -125,8 +132,8 @@ public class RFIDEvent {
     public String toString()
     {
         return (String.format(
-                "Store:  %s, Reader Id:  %s, Tag id: %s, Event Time: %s",
-                getStoreNumber(),  getReceiverId(), getTagId(), getEventTime()));
+                "Store:  %s, Reader Id:  %s, Tag id: %s, Event Time: %s, UPC: %s, Matched: %B",
+                getStoreNumber(),  getReceiverId(), getTagId(), getEventTime().toString(), getUpc(), getMatched()));
 
     }
 
--- src/main/java/services/EventService.java
+++ src/main/java/services/EventService.java
@@ -1,5 +1,7 @@
 package services;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.cloud.bigquery.*;
 import com.google.appengine.api.datastore.Entity;
 import org.joda.time.DateTime;
@@ -17,11 +19,11 @@ public class EventService {
         {
             put("np", "SELECT video_url, tag_id, reader_id, upc, event_timestamp, curr_ts, " +
                     "product_price, location, exit_event, event_status, product_image_url, " +
-                    "product_name, store, matched, check_count, signal FROM `rfid-data-display.rfid_table.event`" +
+                    "product_name, store, matched, check_count, signal FROM `rfid-data-display.rfid_table.event_copy`" +
                     "WHERE check_count < 2 AND matched = false");
             put("pr", "SELECT video_url, tag_id, reader_id, upc, event_timestamp, curr_ts, " +
                     "product_price, location, exit_event, event_status, product_image_url, " +
-                    "product_name, store, matched, check_count, signal FROM `rfid-data-display.rfid_table.event`" +
+                    "product_name, store, matched, check_count, signal FROM `rfid-data-display.rfid_table.event_copy`" +
                     "WHERE check_count < 2 AND matched = false");
         }
     };
@@ -29,44 +31,42 @@ public class EventService {
     private static final Map<String, String> BQ_SALES_BY_LCP = new HashMap<String, String>() {
 
         {
-            put("np", "SELECT STR_NBR, SLS_TS_LOCAL, DTL.UPC_CD, DTL.SKU_NBR,  DTL.UNT_SLS, DTL.CURR_RETL_AMT, POS_TRANS_TYP_CD,  "
-                    + "POS_TRANS_ID, RGSTR_NBR FROM `np-edw-views-thd.SLS.POS_SLS_TRANS_DTL`, UNNEST(DTL) DTL "
+            put("np", "SELECT STR_NBR, SLS_TS_LOCAL, UPC_CD, SKU_NBR,  UNT_SLS, CURR_RETL_AMT, POS_TRANS_TYP_CD,  "
+                    + "POS_TRANS_ID, RGSTR_NBR FROM `rfid-data-display.rfid_table.sales` "
                     + "WHERE STR_NBR = '@storeNumber' AND SLS_TS_LOCAL "
-                    + "BETWEEN  '@startTime' AND '@endTime' AND DTL.UPC_CD IN ('@upc')");
-            put("pr", "SELECT STR_NBR, SLS_TS_LOCAL, DTL.UPC_CD, DTL.SKU_NBR,  DTL.UNT_SLS, DTL.CURR_RETL_AMT, POS_TRANS_TYP_CD,  "
-                    + "POS_TRANS_ID, RGSTR_NBR FROM `pr-edw-views-thd.SLS.POS_SLS_TRANS_DTL`, UNNEST(DTL) DTL "
+                    + "BETWEEN  '@startTime' AND '@endTime' AND UPC_CD IN ('@upc')");
+            put("pr", "SELECT STR_NBR, SLS_TS_LOCAL, UPC_CD, SKU_NBR,  UNT_SLS, CURR_RETL_AMT, POS_TRANS_TYP_CD,  "
+                    + "POS_TRANS_ID, RGSTR_NBR FROM `rfid-data-display.rfid_table.sales` "
                     + "WHERE STR_NBR = '@storeNumber' AND SLS_TS_LOCAL "
-                    + "BETWEEN  '@startTime' AND '@endTime' AND DTL.UPC_CD IN ('@upc')");
+                    + "BETWEEN  '@startTime' AND '@endTime' AND UPC_CD IN ('@upc')");
+        }
+    };
+
+    //write event to BQ for analytics purposes
+    private static final Map<String, String> BQ_EVENT_LCP = new HashMap<String, String>() {
+        {//FIXME CHANGE TO UPDATES
+            put("np", "INSERT INTO `rfid-data-display.rfid_table.event_copy` "
+                    + "(CURR_TS, READER_ID, EVENT_STATUS, EVENT_TIMESTAMP, EXIT_EVENT, "
+                    + "LOCATION, PRODUCT_IMAGE_URL, PRODUCT_NAME, PRODUCT_PRICE, UPC, STORE, "
+                    + " TAG_ID, VIDEO_URL, MATCHED, REGISTER) "
+                    + "VALUES ('@currTime', '@readerId', '@eventStatus', '@eventTimestamp', @exitEvent, "
+                    + "'@location', '@productImageUrl', '@productName', @productPrice, '@upc', "
+                    + "'@store', '@tagId', '@videoUrl', @matched, '@register')");
+            put("pr", "INSERT INTO `rfid-data-display.rfid_table.event_copy` "
+                    + "(CURR_TS, READER_ID, EVENT_STATUS, EVENT_TIMESTAMP, EXIT_EVENT, "
+                    + "LOCATION, PRODUCT_IMAGE_URL, PRODUCT_NAME, PRODUCT_PRICE, UPC, STORE, "
+                    + " TAG_ID, VIDEO_URL, MATCHED, REGISTER) "
+                    + "VALUES ('@currTime', '@readerId', '@eventStatus', '@eventTimestamp', @exitEvent, "
+                    + "'@location', '@productImageUrl', '@productName', @productPrice, '@upc', "
+                    + "'@store', '@tagId', '@videoUrl', @matched, '@register')");
         }
     };
 
     //lcp = life cycle process
     public TableResult getEventData(String lcp){
-        StringBuilder returnString = new StringBuilder();//TODO remove?
         TableResult result = null;
         try {
             result = EventService.runNamed(BQ_RFID_EVENTS_BY_LCP.get(lcp));
-            if (result.getTotalRows() > 0) {
-                for (List<FieldValue> rowDt : result.iterateAll()) {
-                    if (rowDt.get(0).getValue() != null) {
-                        returnString.append(rowDt.get(0).getValue().toString() + " ");
-                        returnString.append(rowDt.get(1).getValue().toString() + " ");
-                        returnString.append(rowDt.get(2).getValue().toString() + " ");
-                        returnString.append(rowDt.get(3).getValue().toString() + " ");
-                        returnString.append(rowDt.get(4).getValue().toString() + " ");
-                        returnString.append(rowDt.get(5).getValue().toString() + " ");
-                        returnString.append(rowDt.get(6).getValue().toString() + " ");
-                        returnString.append(rowDt.get(7).getValue().toString() + " ");
-                        returnString.append(rowDt.get(8).getValue().toString() + " ");
-                        returnString.append(rowDt.get(9).getValue().toString() + " ");
-                        returnString.append(rowDt.get(10).getValue().toString() + " ");
-                        returnString.append(rowDt.get(11).getValue().toString() + " ");
-                        returnString.append(rowDt.get(12).getValue().toString() + " ");
-                        returnString.append(rowDt.get(13).getValue().toString() + " ");
-                        returnString.append(rowDt.get(14).getValue().toString() + " ");
-                    }
-                }
-            }
         }catch (Exception e) {
             e.printStackTrace();
         }
@@ -104,23 +104,23 @@ public class EventService {
 
     public Entity analyzeEvent(RFIDEvent event, String lcp) throws Exception {
 
-
         String register = null;
-        DateTime eventTime;
+        //DateTime eventTime;
 
         //convert event time to proper format for datastore/bq
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-        eventTime = formatter.parseDateTime(event.getEventTime());
-        Date eventTs = new Date(eventTime.getMillis());
+        //DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        //eventTime = formatter.parseDateTime(event.getEventTime());
+        Date eventTs = new Date(event.getEventTime().getMillis());
 
         //query sales if exit reader
         if (event.getExitReader()) {
-            DateTime endTime = eventTime.plusMinutes(10);
+            DateTime endTime = event.getEventTime().plusMinutes(10);
             // *** create the formatter with the "no-millis" format - is there a better way to do this???
             DateTimeFormatter formatterNoMillis = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
             String endTimeNoMillis = endTime.toString(formatterNoMillis);
-            DateTime startTime = eventTime.minusMinutes(20);
+            DateTime startTime = event.getEventTime().minusMinutes(20);
             String startTimeNoMillis = startTime.toString(formatterNoMillis);
+
             HashMap<String, Object> salesProps = lookupMatchingSales(event.getStoreNumber(), startTimeNoMillis, endTimeNoMillis, event.getUpc(), lcp);
             register = (String) salesProps.get("register");
 
@@ -145,7 +145,6 @@ public class EventService {
                 event.getCurrRetailAmt(),
                 event.getUpc(),
                 event.getStoreNumber(),
-                event.getAsciiTag(),
                 event.getTagId(),
                 register,
                 event.getCheckedCounter(),
@@ -153,31 +152,41 @@ public class EventService {
                 );
     }
 
-
-
     private RFIDEvent parseRFIDEvent(List<FieldValue> eventRow) {
 //TODO build out remaining fields
         if (eventRow != null) {
+            DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+            DateTime eventTime = formatter.parseDateTime("2017-05-08 14:54:46");
             return new RFIDEvent(
                     (eventRow.get(1).getValue() != null ? eventRow.get(1).getValue().toString() : null),
                     (eventRow.get(2).getValue() != null ? eventRow.get(2).getValue().toString(): null),
                     (eventRow.get(12).getValue() != null ? eventRow.get(12).getValue().toString(): null),
-                    (eventRow.get(4).getValue() != null ? eventRow.get(4).getValue().toString(): null),
-                    (eventRow.get(15).getValue() != null ? eventRow.get(15).getValue().toString(): null));
+                    eventTime,
+                    Integer.parseInt("56"),
+                    "TOOL_RENTAL",
+                    true,
+                    "313708118953",
+                    "Saw",
+                    Double.parseDouble("150.33"),
+                    false,
+                    0,
+                    false);
 
         }
+        // (eventRow.get(15).getValue() != null ? eventRow.get(15).getValue().toString(): null)
+        //(eventRow.get(4).getValue() != null ? (DateTime)(eventRow.get(4).getValue()): null),
         return null;
     }
 
 
-    public String processEventData(String lcp) throws IOException {
+        public String processEventData(String lcp) throws IOException {
 
         StringBuilder testStringToReturn = new StringBuilder();
-
+            testStringToReturn.append("IN METHOD   ");
         //read messages from the subscription
         EventService eventService = new EventService();
         TableResult eventList = eventService.getEventData(lcp);
-
+            testStringToReturn.append("BEFORE CONDITIONS   ");
         if (eventList != null && eventList.getTotalRows() > 0) {
             for (List<FieldValue> rowDt : eventList.iterateAll()) {
                 if (rowDt.get(0).getValue() != null) {
@@ -186,18 +195,39 @@ public class EventService {
 
                     try {
                         //parse to json
+                        testStringToReturn.append("BEFORE PARSE   ");
+                        //testStringToReturn.append(Arrays.toString(rowDt.toArray()));
                         rfidEvent = eventService.parseRFIDEvent(rowDt);
+                        testStringToReturn.append("AFTER PARSE   ");
 
-//                        return rfidEvent.toString();//FIXME for testing only
+                        //return rfidEvent.toString();//FIXME for testing only
 
                         //build event entity with enrichments
-                        Entity rfidEntity = eventService.analyzeEvent(rfidEvent, lcp);//TODO
-                        testStringToReturn.append(rfidEntity.toString());//FIXME for testing only
-                        testStringToReturn.append("/n");//FIXME for testing only
+                        Entity saveEntity = eventService.analyzeEvent(rfidEvent, lcp);
+
+                        testStringToReturn.append("AFTER ANALYZE   ");
+                        testStringToReturn.append("ENTITY VALUE curr_ts: "+saveEntity.getProperty("curr_ts")+"   ");
+                        testStringToReturn.append("ENTITY VALUE reader_id: "+saveEntity.getProperty("reader_id")+"   ");
+                        testStringToReturn.append("ENTITY VALUE event_status: "+saveEntity.getProperty("event_status")+"   ");
+                        testStringToReturn.append("ENTITY VALUE event_timestamp: "+saveEntity.getProperty("event_timestamp")+"   ");
+                        testStringToReturn.append("ENTITY VALUE exit_event: "+saveEntity.getProperty("exit_event")+"   ");
+                        testStringToReturn.append("ENTITY VALUE location: "+saveEntity.getProperty("location")+"   ");
+                        testStringToReturn.append("ENTITY VALUE product_image_url: "+saveEntity.getProperty("product_image_url")+"   ");
+                        testStringToReturn.append("ENTITY VALUE product_name: "+saveEntity.getProperty("product_name")+"   ");
+                        testStringToReturn.append("ENTITY VALUE product_price: "+saveEntity.getProperty("product_price")+"   ");
+                        testStringToReturn.append("ENTITY VALUE upc: "+saveEntity.getProperty("upc")+"   ");
+                        testStringToReturn.append("ENTITY VALUE store: "+saveEntity.getProperty("store")+"   ");
+                        testStringToReturn.append("ENTITY VALUE tag_id: "+saveEntity.getProperty("tag_id")+"   ");
+                        testStringToReturn.append("ENTITY VALUE video_url: "+saveEntity.getProperty("video_url")+"   ");
+                        testStringToReturn.append("ENTITY VALUE matched: "+saveEntity.getProperty("matched")+"   ");
+                        testStringToReturn.append("ENTITY VALUE checkedCounter: "+saveEntity.getProperty("checkedCounter")+"   ");
+
+                       // testStringToReturn.append(saveEntity.toString());//FIXME for testing only no idea why this doesnt work
+                        testStringToReturn.append("AFTER PRINT OUT   ");
                         //write event to datastore
-                        //writeEventToDS(rfidEvent);//TODO maybe?
+                        //writeEventToDS(saveEntity);//TODO maybe?
                         //write event to big query
-                        //writeEventToBQ(rfidEvent, lcp);//TODO
+                        writeEventToBQ(saveEntity, lcp);//TODO
                     } catch (Exception ex) {
                         // create the error row
 //                        try {
@@ -208,7 +238,8 @@ public class EventService {
 //                            bqex.printStackTrace();
 //                            LOG.severe(String.format("Got exception writing to BQ Error table.  Exception: %s. Event: %s ", bqex.getMessage(), event));
 //                        }
-                        return "WE GOT AN EXCEPTION ";//FIXME remove
+                        return testStringToReturn.toString();
+                        //return "WE GOT AN EXCEPTION ";//FIXME remove
                     }
                 }
             }
@@ -218,7 +249,7 @@ public class EventService {
     }
 
     public Entity createEventEntity(String readerId, Date eventTime, Boolean exitReader, String location, String productName,
-                                    Double currRetailAmt, String upc, String storeNumber, Boolean asciiTag,
+                                    Double currRetailAmt, String upc, String storeNumber,
                                     String tagId, String register, int checkedCounter, Boolean matched) {
         Entity eventEntity = new Entity("event", UUID.randomUUID().toString());
         //reader info
@@ -283,5 +314,51 @@ public class EventService {
         }
         return matchedData;
 
+    }
+
+    /**
+     * write entity event to datastore
+     *
+     * @param event event entity to be written
+     * @throws Exception on error
+     */
+    private static void writeEventToDS(Entity event) throws Exception {
+        //LOG.info(String.format("Writing event to datastore.  Tagid:   %s ", (String) event.getProperty("tag_id")));//FIXME
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(event);
+    }
+
+    /**
+     * write entity event to big query
+     *
+     * @param event event entity to be written
+     * @param lcp   life cycle phase
+     * @throws Exception on error
+     */
+    private static void writeEventToBQ(Entity event, String lcp) throws Exception {
+//TODO write to multiple locations
+        //LOG.info(String.format("Writing event to bigquery.  Tagid:   %s ", (String) event.getProperty("tag_id")));//FIXME
+        DateTime currTs = new DateTime((Date) event.getProperty("curr_ts"));
+        DateTime eventTs = new DateTime((Date) event.getProperty("event_timestamp"));
+
+        String queryString = BQ_EVENT_LCP.get(lcp).replace("@currTime", currTs.toString())
+                .replace("@readerId", (String) event.getProperty("reader_id"))
+                .replace("@eventStatus", (String) event.getProperty("event_status"))
+                .replace("@eventTimestamp", eventTs.toString())
+                .replace("@exitEvent", Boolean.toString((Boolean) event.getProperty("exit_event")))
+                .replace("@location", (String) event.getProperty("location"))
+                .replace("@productImageUrl", (String) event.getProperty("product_image_url"))
+                .replace("@productName", (String) event.getProperty("product_name"))
+                .replace("@productPrice", Double.toString((Double) event.getProperty("product_price")))
+                .replace("@upc", (String) event.getProperty("upc"))
+                .replace("@tagId", (String) event.getProperty("tag_id"))
+                .replace("@store", (String) event.getProperty("store"))
+                .replace("@videoUrl", (String) event.getProperty("video_url"))
+                .replace("@matched", Boolean.toString((Boolean) event.getProperty("matched")))//FIXME is this coming across?
+                .replace("@register", (String) event.getProperty("register"));
+
+
+        // Instantiates a client
+        TableResult result = runNamed(queryString);
     }
 }
